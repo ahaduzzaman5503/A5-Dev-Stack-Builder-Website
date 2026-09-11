fcad3e62033ae57c26b2b7f6c05910a2d51d6c3d
unknown <ahaduzzaman5503@gmail.com>
2026-09-11 19:55:24 +0600

    deploy the porject on netlify and added the live link

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,29 @@ import Technologies from "./Componants/Technologies";
 import Footer from "./Componants/Footer";
 
 const tecnologyFetch = async (): Promise<Itecnology[]> => {
-  const res = await fetch("/public/tech_stack_data.json");
+  const res = await fetch("/tech_stack_data.json");
   const data = await res.json();
   return data;
 };
 
 function App() {
   const tecnologyPromise = tecnologyFetch();
+
   return (
     <>
       <Nevber></Nevber>
       <Bannar></Bannar>
+
       <Suspense
         fallback={
           <span className="loading loading-spinner text-secondary"></span>
         }
       >
-        <Technologies tecnologyPromise={tecnologyPromise}></Technologies>
+        <Technologies
+          tecnologyPromise={tecnologyPromise}
+        ></Technologies>
       </Suspense>
+
       <Footer></Footer>
     </>
   );
